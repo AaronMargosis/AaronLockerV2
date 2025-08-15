4793b233f5f1a4dbf42bd58df63fec0cc84830d6
AaronMargosis <24351128+AaronMargosis@users.noreply.github.com>
2025-08-15 13:50:32 -0400

    Work around bugs in PowerShell

--- Scripts/ConfigureForAppLocker.ps1
+++ Scripts/ConfigureForAppLocker.ps1
@@ -10,13 +10,33 @@ Sets the maximum log size for each of the AppLocker event logs to 1GB.
 
 #>
 
-# Configure AppIDSvc for Automatic start
-Set-Service -Name AppIDSvc -StartupType Automatic
+# Check for admin rights first
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    Write-Error "This script requires administrative rights."
+    exit
+}
+
+# Configure AppIDSvc for Automatic start if not already configured that way
+if ((Get-Service AppIDSvc).StartType -ne 'Automatic')
+{
+    # There's a bug in PS v5.1 through at least v7.5.2 where setting the start type reports an error,
+    # even though it succeeds, so set the error action to suppress that error and then verify
+    # success afterward.
+    Set-Service -Name AppIDSvc -StartupType 'Automatic' -ErrorAction SilentlyContinue
+    if ((Get-Service AppIDSvc).StartType -ne 'Automatic')
+    {
+        Write-Warning ("Failed to set service start type to Automatic. Currently " + (Get-Service AppIDSvc).StartType)
+    }
+}
 
 # Start the service if not already running
-Start-Service -Name AppIDSvc
+if ((Get-Service AppIDSvc).Status -ne 'Running')
+{
+    Start-Service -Name AppIDSvc
+}
 
-# Set the primary AppLocker event log sizes to 1GB
+# Set the primary AppLocker event log sizes to 1GB each.
 
 $logName = 'Microsoft-Windows-AppLocker/EXE and DLL'
 $log = New-Object System.Diagnostics.Eventing.Reader.EventLogConfiguration $logName
